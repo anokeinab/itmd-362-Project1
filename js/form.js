29ef369f70993eb798b56e38cbde2a990b211630
Divin Gregis Baniekona <31362277+anokeinab@users.noreply.github.com>
2018-03-13 19:13:44 -0500

    Update form.js
    create a function to check if the email field is filled out by the user

--- js/form.js
+++ js/form.js
@@ -1,1 +1,8 @@
-
+<script>
+function validateForm() {
+    var x = document.forms["form"]["email"].value;
+    if (x == "") {
+        alert("Please type your email");
+        return false;
+    }
+}
